0bb82cfdb204020024a3d77f52501e305178bf93
MoMaRa <hexe-monja@web.de>
2014-06-11 10:40:03 +0200

    hüpfburgbetreiber gegen keks getauscht

--- src/de/rwth_aachen/kbsg/dq/HelloWorld.java
+++ src/de/rwth_aachen/kbsg/dq/HelloWorld.java
@@ -2,7 +2,7 @@ package de.rwth_aachen.kbsg.dq;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Wo ist denn der Huepfburgenbetreiber?");
+		System.out.println("Wo ist denn der Keks?");
 	}
 
 }
